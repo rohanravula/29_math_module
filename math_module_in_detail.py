e7609146a7b2319638358e0cc31d2ca2d2738e41
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-08-27 22:28:30 +0530

    Add files via upload

---
+++ math_module_in_detail.py
@@ -0,0 +1,36 @@
+"ceil" "whcih means rounding upto next value"
+# import math as c
+# print(c.ceil(0.2)) #1
+# print(c.ceil(3.6)) #4
+# print(c.ceil(8.2)) #9
+# print(c.ceil(-5.3)) #-5
+# print(c.ceil(-0.4)) #0
+# print(c.ceil(30.87)) #31
+
+"floor" "whcih means rounding upto to decrease value"
+# import math as f
+
+# print(f.floor(8.6)) #8
+# print(f.floor(36.2)) #36
+# print(f.floor(-6.5)) #-7
+# print(f.floor(-3.2)) #-4
+
+"pi value"
+# import math as p
+# print(p.pi)
+# print(p.pi**2)
+
+"squre root" "which means to know the square root of a certain number"
+# import math as sq
+# print(sq.sqrt(4)) #2.0
+# print(sq.sqrt(38)) #6.164414002968976 .if we find the sq. root in python the answer is shown in terms of float
+# print(sq.sqrt(6.94)) #2.6343879744638983
+
+"% this is known as modulus or percentail. whcih gives us the remainder"
+# g=17%3
+# print(g) #2. the remainder is 2 when we do division of 17/3 
+
+"trunc which means it cuts the decimal values"
+import math as t
+print(t.trunc(5.67)) #5
+print(t.trunc(-9.748)) #-9
